9231b310ec85a88c48fcb5b4dd2fc9191eb5aca4
Lisa Kester <lkester@athenahealth.com>
2021-04-14 22:07:38 -0500

    Adding initial files from guide.  Adding gitignore

---
+++ TestService.Tests/TestService_IsPrimeShould.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using System;
+using TestService;
+
+namespace TestService.Tests
+{
+    public class TestService_IsPrimeShould
+    {
+        public class PrimeService_IsPrimeShould
+        {
+            [Fact]
+            public void IsPrime_InputIs1_ReturnFalse()
+            {
+                var primeService = new TestService.PrimeService();
+                bool result = primeService.IsPrime(1);
+
+                Assert.False(result, "1 should not be prime");
+            }
+        }
+    }
+}
---
+++ TestService/TestService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestService
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            throw new NotImplementedException("Not implemented.");
+        }
+    }
+}
