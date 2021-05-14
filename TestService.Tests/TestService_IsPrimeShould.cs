14262724a59b7bfaa2a92d1c818389646778379d
Lisa Kester <lkester@athenahealth.com>
2021-05-13 21:38:59 -0500

    Added code from swaps practice

--- TestService.Tests/TestService_IsPrimeShould.cs
+++ TestService.Tests/TestService_IsPrimeShould.cs
@@ -6,34 +6,59 @@ namespace TestService.Tests
 {
     public class TestService_IsPrimeShould
     {
-        public class PrimeService_IsPrimeShould
-        {
-            private PrimeService _primeService;
+        public class SwapService_IsSwappedRight {
 
-            public PrimeService_IsPrimeShould()
-            {
-                this._primeService = new PrimeService();
+            private SwapsService _swapsService;
+
+            public SwapService_IsSwappedRight() {
+                this._swapsService = new SwapsService();
             }
 
+            /// <summary>
+            /// 5,5,5,5,5 should result in 0,5,5
+            /// </summary>
             [Fact]
-            public void IsPrime_InputIs1_ReturnFalse()
+            public void AreSwapResultsCorrect()
             {
-                var primeService = new PrimeService();
-                bool result = primeService.IsPrime(1);
 
-                Assert.False(result, "1 should not be prime");
+                int[] a = new int[] { 5, 5, 5, 5, 5 };
+                var result = _swapsService.countTheSwaps(a);
+                var expectedResult = new SwapInfo(0,5,5);
+
+                Assert.True(expectedResult.Equals(result));
+
             }
 
-            [Theory]
-            [InlineData(-1)]
-            [InlineData(0)]
-            [InlineData(1)]
-            public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
-            {
-                var result = _primeService.IsPrime(value);
-
-                Assert.False(result, $"{value} should not be prime");
-            }
         }
+
+        //public class PrimeService_IsPrimeShould
+        //{
+        //    private SwapsService _primeService;
+
+        //    public PrimeService_IsPrimeShould()
+        //    {
+        //        this._primeService = new SwapsService();
+        //    }
+
+        //    [Fact]
+        //    public void IsPrime_InputIs1_ReturnFalse()
+        //    {
+        //        var primeService = new SwapsService();
+        //        bool result = primeService.IsPrime(1);
+
+        //        Assert.False(result, "1 should not be prime");
+        //    }
+
+        //    [Theory]
+        //    [InlineData(-1)]
+        //    [InlineData(0)]
+        //    [InlineData(1)]
+        //    public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+        //    {
+        //        var result = _primeService.IsPrime(value);
+
+        //        Assert.False(result, $"{value} should not be prime");
+        //    }
+        //}
     }
 }
--- TestService/TestService.cs
+++ TestService/TestService.cs
@@ -2,16 +2,97 @@
 
 namespace TestService
 {
-    public class PrimeService
+
+    class Solution
     {
-        public bool IsPrime(int candidate)
+        // Complete the countSwaps function below.
+        public static void countSwaps(int[] a)
         {
-            if (candidate == 1)
-            {
-                return false;
-            }
-            throw new NotImplementedException("Not fully implemented.");
+            var swapsService = new SwapsService();
+            swapsService.countTheSwaps(a);
         }
 
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), aTemp => Convert.ToInt32(aTemp));
+            countSwaps(a);
+        }
+    }
+
+    public class SwapInfo
+    {
+        public int Count { get; set; }
+        public int StartNum { get; set; }
+        public int EndNum { get; set; }
+
+        public SwapInfo()
+        {
+            this.Count = -1;
+            this.StartNum = -1;
+            this.EndNum = -1;
+        }
+
+        public SwapInfo(int count, int startNum, int endNum)
+        {
+            this.Count = count;
+            this.StartNum = startNum;
+            this.EndNum = endNum;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is SwapInfo result &&
+                   Count == result.Count &&
+                   StartNum == result.StartNum &&
+                   EndNum == result.EndNum;
+        }
+
+    }
+
+    public class SwapsService
+    {
+
+        public SwapInfo countTheSwaps(int[] a)
+        {
+            var result = new SwapInfo();
+
+            if (a == null || a.Length < 1)
+            {
+                Console.WriteLine("Array is empty or null");
+                return result;
+            }
+
+            // This is a valid case, so set to 0 swaps
+            result.Count = 0;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+
+                for (int j = 0; j < a.Length - 1; j++)
+                {
+                    // Swap adjacent elements if they are in decreasing order
+                    if (a[j] > a[j + 1])
+                    {
+
+                        // swap the values
+                        int tempA = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = tempA;
+                        result.Count++;
+                    }
+                }
+            }
+
+            result.StartNum = a[0];
+            result.EndNum = a[a.Length - 1];
+
+            Console.WriteLine(String.Format("Array is sorted in {0} swaps.", result.Count));
+            Console.WriteLine(String.Format("First Element: {0}", a[0]));
+            Console.WriteLine(String.Format("Last Element: {0}", a[a.Length - 1]));
+
+            return result;
+        }
     }
 }
