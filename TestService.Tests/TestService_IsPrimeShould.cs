d4d99177c5fec22d220be8b86bf016aeb04088ff
Lisa Kester <lkester@athenahealth.com>
2021-05-15 11:34:40 -0500

    Solved Ransom Note challenge

--- TestService.Tests/TestService_IsPrimeShould.cs
+++ TestService.Tests/TestService_IsPrimeShould.cs
@@ -8,32 +8,35 @@ namespace TestService.Tests
     {
         public class PrimeService_IsPrimeShould
         {
-            private PrimeService _primeService;
 
-            public PrimeService_IsPrimeShould()
-            {
-                this._primeService = new PrimeService();
-            }
+           
 
-            [Fact]
-            public void IsPrime_InputIs1_ReturnFalse()
-            {
-                var primeService = new PrimeService();
-                bool result = primeService.IsPrime(1);
+            //private PrimeService _primeService;
 
-                Assert.False(result, "1 should not be prime");
-            }
+            //public PrimeService_IsPrimeShould()
+            //{
+            //    this._primeService = new PrimeService();
+            //}
 
-            [Theory]
-            [InlineData(-1)]
-            [InlineData(0)]
-            [InlineData(1)]
-            public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
-            {
-                var result = _primeService.IsPrime(value);
+            //[Fact]
+            //public void IsPrime_InputIs1_ReturnFalse()
+            //{
+            //    var primeService = new PrimeService();
+            //    bool result = primeService.IsPrime(1);
 
-                Assert.False(result, $"{value} should not be prime");
-            }
+            //    Assert.False(result, "1 should not be prime");
+            //}
+
+            //[Theory]
+            //[InlineData(-1)]
+            //[InlineData(0)]
+            //[InlineData(1)]
+            //public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+            //{
+            //    var result = _primeService.IsPrime(value);
+
+            //    Assert.False(result, $"{value} should not be prime");
+            //}
         }
     }
 }
--- TestService/TestService.cs
+++ TestService/TestService.cs
@@ -1,17 +1,87 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
 namespace TestService
 {
-    public class PrimeService
+    class Result
     {
-        public bool IsPrime(int candidate)
+
+        /*
+         * Complete the 'checkMagazine' function below.
+         *
+         * The function accepts following parameters:
+         *  1. STRING_ARRAY magazine
+         *  2. STRING_ARRAY note
+         */
+
+        public static void checkMagazine(List<string> magazine, List<string> note)
         {
-            if (candidate == 1)
+            var magWords = countWords(magazine);
+            var noteWords = countWords(note);
+
+            foreach (var word in noteWords.Keys)
             {
-                return false;
+                //Console.WriteLine("Word: " + word + " Count: " + noteWords[word]);
+                //Console.WriteLine("magWord: " + word + " Count: " + magWords[word]);
+
+                if (!magWords.ContainsKey(word) || noteWords[word] > magWords[word])
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+                
             }
-            throw new NotImplementedException("Not fully implemented.");
+            Console.WriteLine("Yes");
+        }
+
+        private static Dictionary<string, int> countWords(List<string> words)
+        {
+            var result = new Dictionary<string, int>(words.Count);
+            foreach (var word in words)
+            {
+                //Console.WriteLine("Current word: " + word);
+                if (!result.ContainsKey(word))
+                {
+                    //Console.WriteLine("Added word: " + word);
+                    result.Add(word, 1);
+                }
+                else
+                {
+                    //Console.WriteLine("Incremented word: " + word);
+                    result[word] = result[word] + 1;
+                }
+            }
+
+            return result;
         }
 
     }
+
+    public class Solution
+    {
+        public static void Main()
+        {
+
+            string magazineWords = "two times three is not four";
+            string noteWords = "two times two is four";
+
+            List<string> magazine = magazineWords.TrimEnd().Split(' ').ToList();
+
+            List<string> note = noteWords.TrimEnd().Split(' ').ToList();
+
+            Result.checkMagazine(magazine, note);
+        }
+    }
+
 }
