dd048a37a857aa92d1211360334d6f867ba81c96
Lisa Kester <lkester@athenahealth.com>
2021-05-15 21:04:38 -0500

    Added a dictionary to track occurences so the search would also be O(1).

--- TestService/TestService.cs
+++ TestService/TestService.cs
@@ -17,13 +17,41 @@ namespace TestService
     class Solution
     {
         private Dictionary<int, int> _data;
+        Dictionary<int, HashSet<int>> _occurences;
 
         public Solution()
         {
+            // Key: number (data), Value: number of occurences
             _data = new Dictionary<int, int>();
+
+            // Key: Num of occurences, Value: List of numbers that happened that many times
+            // _data and occurences must be kept up to date together.
+            _occurences = new Dictionary<int, HashSet<int>>();
 
         }
 
+        void updateOccurences(int entry, int oldOccurence, int newOccurence )
+        {
+            // If there were no occurences of the number before and there's
+            // less now, then this was a delete. No need to delete something empty.
+            if (newOccurence < 0)
+                return;
+
+            // Remove the entry from the occurence list. It should always exist.
+            if (_occurences.ContainsKey(oldOccurence))
+            {
+                _occurences[oldOccurence].Remove(entry);
+            }
+
+            // Create the list of entries that happen "newOccurence" times
+            if(!_occurences.ContainsKey(newOccurence))
+            {
+                _occurences[newOccurence] = new HashSet<int>();
+            }
+
+            // Add the current entry to the list of entries that happend "newOccurence" times
+            _occurences[newOccurence].Add(entry);
+        }
 
         void addEntry(int entry)
         {
@@ -31,12 +59,16 @@ namespace TestService
 
             if (_data.ContainsKey(entry))
             {
+                updateOccurences(entry, _data[entry], _data[entry] + 1);
                 _data[entry] = _data[entry] + 1;
+
             }
             else
             {
                 // Create an entry if it doesn't exist
+                updateOccurences(entry, 0, 1);
                 _data.Add(entry, 1);
+                
             }
         }
 
@@ -45,7 +77,10 @@ namespace TestService
             Console.WriteLine("Delete:" + entry);
 
             if (_data.ContainsKey(entry) && _data[entry] > 0)
-                _data[entry] = _data[entry] - 1;
+            {
+                updateOccurences(entry, _data[entry], _data[entry] - 1);
+                _data[entry] = _data[entry] - 1;               
+            }
         }
 
         // print 1 if any integer is present with x occurences
@@ -53,7 +88,7 @@ namespace TestService
         {
             Console.WriteLine("Query:" + occureces);
 
-            if (_data.ContainsValue(occureces))
+            if (_occurences.ContainsKey(occureces) && _occurences[occureces].Count > 0)
                 return 1;
             return 0;
         }
