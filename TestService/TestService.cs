2fac871dfbb6efaa69af800d8c78903fb2790e2d
Lisa Kester <lkester@athenahealth.com>
2021-05-16 16:26:12 -0500

    Works for most cases, but too slow and r=1 is an issue

--- TestService/TestService.cs
+++ TestService/TestService.cs
@@ -12,76 +12,134 @@ using System.Text.RegularExpressions;
 using System.Text;
 using System;
 
-namespace TestService
+class Solution
 {
-    class Result
+    // This function requires that the first entry exists. O(1)  
+    static List<List<long>> getNextEntry(List<long> triplet, Dictionary<long, int> numberCounts, long r)
     {
 
-        /*
-         * Complete the 'checkMagazine' function below.
-         *
-         * The function accepts following parameters:
-         *  1. STRING_ARRAY magazine
-         *  2. STRING_ARRAY note
-         */
+        //Console.WriteLine("There");
 
-        public static void checkMagazine(List<string> magazine, List<string> note)
+        var result = new List<List<long>>();
+
+        // Look at the last entry and see how many next entries we can create
+        var lastEntry = triplet[triplet.Count - 1];
+        var nextEntry = lastEntry * r;
+        var numOccurences = 0;
+
+        if (numberCounts.ContainsKey(nextEntry))
         {
-            var magWords = countWords(magazine);
-            var noteWords = countWords(note);
-
-            foreach (var word in noteWords.Keys)
-            {
-                //Console.WriteLine("Word: " + word + " Count: " + noteWords[word]);
-                //Console.WriteLine("magWord: " + word + " Count: " + magWords[word]);
-
-                if (!magWords.ContainsKey(word) || noteWords[word] > magWords[word])
-                {
-                    Console.WriteLine("No");
-                    return;
-                }
-                
-            }
-            Console.WriteLine("Yes");
+            numOccurences = numberCounts[nextEntry];
         }
-
-        private static Dictionary<string, int> countWords(List<string> words)
-        {
-            var result = new Dictionary<string, int>(words.Count);
-            foreach (var word in words)
-            {
-                //Console.WriteLine("Current word: " + word);
-                if (!result.ContainsKey(word))
-                {
-                    //Console.WriteLine("Added word: " + word);
-                    result.Add(word, 1);
-                }
-                else
-                {
-                    //Console.WriteLine("Incremented word: " + word);
-                    result[word] = result[word] + 1;
-                }
-            }
-
+        else
             return result;
+
+        triplet.Add(nextEntry);
+
+        for (int i = 0; i < numOccurences; i++)
+        {
+            // Yes, there will be duplicates, that's the point
+            result.Add(new List<long>(triplet));
         }
 
+        //Console.WriteLine("Got something");
+
+        //foreach(var t in result)
+        //{
+        //    foreach (var item in t)
+        //    {
+        //        Console.WriteLine("Item: " + item);
+        //    }
+        //}
+
+        return result;
     }
 
-    public class Solution
+    // Complete the countTriplets function below.
+    /* 
+    Go through the list of numbers and remove anything that isn't divisible by 3.  
+    Create result lists List<List<int>>.  This will store the valid touples 
+     
+     */
+    static long countTriplets(List<long> arr, long r)
     {
-        public static void Main()
+        int tripletCount = 0;
+
+        // This tracks the number of occurences so I can find the next entry
+        var numberCounts = new Dictionary<long, int>(arr.Count);
+
+        // This tracks the numbers themselves so I can start the tuples
+
+
+        // O(n)
+        // The input only gives sorted info that is divisible by r.  Add it to the dictionary and track how many there are.
+        foreach(var entry in arr)
         {
 
-            string magazineWords = "two times three is not four";
-            string noteWords = "two times two is four";
-
-            List<string> magazine = magazineWords.TrimEnd().Split(' ').ToList();
-
-            List<string> note = noteWords.TrimEnd().Split(' ').ToList();
-
-            Result.checkMagazine(magazine, note);
+            if (!numberCounts.ContainsKey(entry))
+                numberCounts[entry] = 1;
+            else
+                numberCounts[entry] = numberCounts[entry] + 1;
         }
+
+        // To support special case if r=1.  1 will be the only valid number.  Need to remove 1 from the numberCounts
+        if (numberCounts.ContainsKey(1))
+        {
+            numberCounts[1] = numberCounts[1] - 1;
+        }
+
+        // Stores what we are working on
+        var tripletLists = new Stack<List<long>>();
+
+        // Create tuples 
+        foreach(var entry in arr)
+        {
+            //Console.WriteLine("Entry: " + entry);
+
+            var tuple = new List<long>(3);
+            tuple.Add(entry);
+            tripletLists.Push(tuple);
+        }
+
+        while (tripletLists.Count > 0)
+        {
+
+            var triplet = tripletLists.Pop();
+            var possibleTriplets = getNextEntry(triplet, numberCounts, r);
+
+            foreach(var t in possibleTriplets)
+            {
+                //Console.WriteLine("Here");
+
+                // If we found a valid triplet, then count it.  Otherwise, add it onto the stack to find the next numbers.
+                if (t.Count == 3)
+                    tripletCount++;
+                else
+                    tripletLists.Push(t);
+            }
+
+        }
+        return tripletCount;
+
     }
 
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] nr = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(nr[0]);
+
+        long r = Convert.ToInt64(nr[1]);
+
+        List<long> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt64(arrTemp)).ToList();
+
+        long ans = countTriplets(arr, r);
+
+        textWriter.WriteLine(ans);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
 }
