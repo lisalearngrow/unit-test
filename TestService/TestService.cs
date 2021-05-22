522e3b8e09a4c7a005ad7a7cddf45ab9562b87af
Lisa Kester <lkester@athenahealth.com>
2021-05-22 15:44:33 -0500

    Adding initial common child approach.  Need to try something else

--- TestService/TestService.cs
+++ TestService/TestService.cs
@@ -12,139 +12,142 @@ using System.Text.RegularExpressions;
 using System.Text;
 using System;
 
-namespace TestService
+class Result
 {
-    class Solution
+
+    /*
+     * Complete the 'commonChild' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING s1
+     *  2. STRING s2
+     */
+
+    public static int commonChild(string s1, string s2)
     {
-        private Dictionary<int, int> _data;
-        Dictionary<int, HashSet<int>> _occurences;
+        var resultCount = 0;
 
-        public Solution()
+        Console.WriteLine("length of string 1: " + s1.Length);
+        Console.WriteLine("length of string 2: " + s2.Length);
+
+        // Store the unique letters of the first string
+        var string1Occurences = new Dictionary<char, int>();
+        foreach (char letter in s1)
         {
-            // Key: number (data), Value: number of occurences
-            _data = new Dictionary<int, int>();
-
-            // Key: Num of occurences, Value: List of numbers that happened that many times
-            // _data and occurences must be kept up to date together.
-            _occurences = new Dictionary<int, HashSet<int>>();
-
-        }
-
-        void updateOccurences(int entry, int oldOccurence, int newOccurence )
-        {
-            // If there were no occurences of the number before and there's
-            // less now, then this was a delete. No need to delete something empty.
-            if (newOccurence < 0)
-                return;
-
-            // Remove the entry from the occurence list. It should always exist.
-            if (_occurences.ContainsKey(oldOccurence))
+            if (!string1Occurences.ContainsKey(letter))
             {
-                _occurences[oldOccurence].Remove(entry);
-            }
-
-            // Create the list of entries that happen "newOccurence" times
-            if(!_occurences.ContainsKey(newOccurence))
-            {
-                _occurences[newOccurence] = new HashSet<int>();
-            }
-
-            // Add the current entry to the list of entries that happend "newOccurence" times
-            _occurences[newOccurence].Add(entry);
-        }
-
-        void addEntry(int entry)
-        {
-            Console.WriteLine("Add:" + entry);
-
-            if (_data.ContainsKey(entry))
-            {
-                updateOccurences(entry, _data[entry], _data[entry] + 1);
-                _data[entry] = _data[entry] + 1;
-
+                string1Occurences.Add(letter, 1);
             }
             else
             {
-                // Create an entry if it doesn't exist
-                updateOccurences(entry, 0, 1);
-                _data.Add(entry, 1);
-                
+                string1Occurences[letter] = string1Occurences[letter] + 1;
             }
         }
 
-        void deleteEntry(int entry)
+        var stringTwoOccurences = new Dictionary<char, int>();
+        foreach (char letter in s2)
         {
-            Console.WriteLine("Delete:" + entry);
-
-            if (_data.ContainsKey(entry) && _data[entry] > 0)
+            if (!stringTwoOccurences.ContainsKey(letter))
             {
-                updateOccurences(entry, _data[entry], _data[entry] - 1);
-                _data[entry] = _data[entry] - 1;               
+                stringTwoOccurences.Add(letter, 1);
+            }
+            else
+            {
+                stringTwoOccurences[letter] = stringTwoOccurences[letter] + 1;
             }
         }
 
-        // print 1 if any integer is present with x occurences
-        int queryEntry(int occureces)
-        {
-            Console.WriteLine("Query:" + occureces);
+        // Remove letters not in common from string 1 & 2
+        var newS1 = "";
+        var newS2 = "";
 
-            if (_occurences.ContainsKey(occureces) && _occurences[occureces].Count > 0)
-                return 1;
-            return 0;
+        foreach(char c in s1)
+        {
+            if (stringTwoOccurences.ContainsKey(c))
+                newS1 = newS1 + c;
+            else
+            {
+                Console.WriteLine("REJECTED: " + c);
+            }
         }
 
-        // Complete the freqQuery function below. Return the _result
-        static List<int> freqQuery(List<List<int>> queries)
+        foreach (char c in s2)
         {
-            var model = new Solution();
-            var result = new List<int>();
-
-            for (int i = 0; i < queries.Count; i++)
+            if (string1Occurences.ContainsKey(c))
+                newS2 = newS2 + c;
+            else
             {
-                int operation = queries[i][0];
-                int num = queries[i][1];
+                Console.WriteLine("REJECTED: " + c);
+            }
+        }
 
-                Console.WriteLine("op:" + operation);
-                Console.WriteLine("num:" + num);
+        Console.WriteLine("length of string 1 occurences: " + newS1.Length);
+        Console.WriteLine("length of string 2: " + newS2.Length);
 
-                switch (operation)
+        var numLettersLookedAt = 0;
+
+        // This string will get smaller and smaller as we look for letters
+        string currentS1 = newS1;
+
+        // This tracks where we are looking at string 1 so we don't start over.
+        int trackingIndex = 0;
+
+        // iterate over the second string
+        for (int i = 0; i < newS2.Length; i++) {
+            var currentLetter = newS2[i];
+            numLettersLookedAt++;
+
+            Console.WriteLine("INDEX: " + trackingIndex);
+
+            Console.WriteLine("looking at: " + currentLetter);
+
+            // check to see if the letter was present in the other string.  This doesn't mean it's still available
+            if (!string1Occurences.ContainsKey(currentLetter))
+                continue;
+
+            // Then check to see if you can find the letter in string 1.  If so, count it
+            for(int currentIndex = trackingIndex; currentIndex < currentS1.Length; currentIndex++)
+            {
+                var l = currentS1[currentIndex];
+
+                Console.WriteLine("Comparing: " + l + " " + currentLetter);
+
+                // Found the letter
+                if (l == currentLetter)
                 {
-                    case 1:
-                        model.addEntry(num);
-                        break;
-                    case 2:
-                        model.deleteEntry(num);
-                        break;
-                    case 3:
-                        result.Add(model.queryEntry(num));
-                        break;
-                    default:
-                        break;
+                    Console.WriteLine("letter found: " + l);
+
+                    // Increased the count
+                    resultCount++;
+
+                    trackingIndex = currentIndex + 1;
+                    break;
                 }
             }
-            return result;
+
         }
 
-        static void Main(string[] args)
-        {
-            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
-            int q = Convert.ToInt32(Console.ReadLine().Trim());
-
-            List<List<int>> queries = new List<List<int>>();
-
-            for (int i = 0; i < q; i++)
-            {
-                queries.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(queriesTemp => Convert.ToInt32(queriesTemp)).ToList());
-            }
-
-            List<int> ans = freqQuery(queries);
-
-            textWriter.WriteLine(String.Join("\n", ans));
-
-            textWriter.Flush();
-            textWriter.Close();
-        }
+        return resultCount;
     }
 
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s1 = Console.ReadLine();
+
+        string s2 = Console.ReadLine();
+
+        int result = Result.commonChild(s1, s2);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
 }
