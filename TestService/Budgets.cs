5ba643b6ac0d3c51a0a3bb5d6019726f370e4c5f
Lisa Kester <lkester@athenahealth.com>
2021-06-11 12:53:53 -0500

    Adding some other practice problems real quick

---
+++ TestService/Budgets.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class PracticeSolution
+{
+    public static double FindGrantsCap(double[] grantsArray, double newBudget)
+    {
+
+        double grantsCap = 0;
+        //double previousCap = 0;
+        bool foundCap = false;
+        double adjustment = 0;
+
+        while (!foundCap)
+        {
+            // Take # of grants and divide the new budget by that
+            grantsCap = (double)newBudget / grantsArray.Length + adjustment;
+
+            double sum = 0;
+
+            // Add together the amount of grants if this was the cap
+            for (int i = 0; i < grantsArray.Length; i++)
+            {
+                var cash = grantsArray[i];
+                if (cash > grantsCap)
+                    cash = grantsCap;
+
+                sum += cash;
+            }
+
+            // If the sum is less than the new budget, then try again
+            if ((newBudget - grantsArray.Length) < sum && sum < (newBudget + grantsArray.Length))
+            {
+                foundCap = true;
+            }
+            else
+            {
+                // How off were we?  Let's try to zero in.
+                adjustment = (double)(newBudget - sum) / (double)grantsArray.Length;
+            }
+        }
+
+        return grantsCap;
+    }
+
+    static void Main(string[] args)
+    {
+
+    }
+}
+
---
+++ TestService/Solution.cs
@@ -0,0 +1,98 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    static bool isSpecial(string current)
+    {
+        Console.WriteLine("Testing: " + current);
+        if (current.Count() == 1)
+            return true;
+
+        if (current.Count() % 2 != 0)
+        {
+            // Middle has -1 due to zero based index
+            int middle = (int)Math.Truncate((decimal)current.Length / 2);
+
+            // Remove the middle letter and check the rest. 
+            current = current.Remove(middle, 1);
+            Console.WriteLine("Modified: " + current);
+        }
+
+        // Check to see if all the letters are the same
+        var tempLetter = current[0];
+        bool isSpecial = true;
+        for (int j = 0; j < current.Length; j++)
+        {
+            if (tempLetter != current[j])
+                isSpecial = false;
+        }
+        return isSpecial;
+    }
+    // Complete the substrCount function below.
+    /*
+        Brute force is to iterate over the string for each count of letters up to the size of the string.  1 letter substrings, 2 letter, 3 letter and throw them all in a list.
+        
+        You don't have to iterate over the whole string more than once, you could look at the next n letters for the current index.  For example aasbcssass,
+        we could start at index 1, add that letter, then get that letter and the next one, then the additional letter.  This would make it easier to stop extra work.
+    */
+
+    static long substrCount(int n, string s)
+    {
+        /* Brute force first */
+        List<string> specialStrings = new List<string>();
+
+        for (int len = 1; len <= s.Length; len++)
+        {
+            // iterate over the whole string
+            string current = "";
+
+            for (int i = 0; (i + len - 1) < s.Length; i++)
+            {
+                // Count the current string if it meets the criteria
+                current = s.Substring(i, len);
+                Console.WriteLine("Current: " + current);
+                if (isSpecial(current))
+                {
+                    specialStrings.Add(current);
+                }
+            }
+        }
+        // What did I get?
+        Console.WriteLine("Final items:");
+        foreach (var item in specialStrings)
+        {
+            Console.WriteLine(item);
+        }
+
+        return specialStrings.Count;
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        string s = Console.ReadLine();
+
+        long result = substrCount(n, s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
